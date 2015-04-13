835a1e2461cbd8625646b4f7196ab1ed1dd09ffa
Jakub Filak <jfilak@redhat.com>
2015-04-13 15:11:46 +0200

    docker: fix reading of journal for kernel oopses
    Docker mounts host's /var/log/journal/$ID to container's
    /var/log/journal
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- abrt-default-cmd.sh
+++ abrt-default-cmd.sh
@@ -4,7 +4,7 @@ echo "Starting abrtd."
 abrtd || exit 1
 
 echo "Starting abrt-dump-journal-oops."
-abrt-dump-journal-oops -a -fxtD &
+abrt-dump-journal-oops -J /host/var/log/journal -fxtD &
 
 echo "Starting abrt-dbus."
 abrt-dbus -t 2147483647
